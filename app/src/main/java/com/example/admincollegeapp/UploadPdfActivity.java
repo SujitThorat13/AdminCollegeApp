43d04425340315caac7c27bca79272e0c110d14e
Sujit <sujit.j.thorat@gmail.com>
2022-03-05 12:58:22 +0530

    Admin-College App

--- app/src/main/java/com/example/admincollegeapp/UploadPdfActivity.java
+++ app/src/main/java/com/example/admincollegeapp/UploadPdfActivity.java
@@ -1,40 +1,57 @@
 package com.example.admincollegeapp;
 
+import static android.provider.OpenableColumns.DISPLAY_NAME;
+
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.icu.text.CaseMap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.provider.OpenableColumns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 
 public class UploadPdfActivity extends AppCompatActivity {
 
     private CardView addPdf;
     private EditText pdfTitle;
     private Button uploadPdfBtn;
+    private TextView pdfTextView;
+    private String pdfName,title;
+
 
 
     private final int REQ =1;
     private Uri pdfData;
-    private DatabaseReference reference;
+    private DatabaseReference databasereference;
     private StorageReference storageReference;
-    String downloadUrl = "";
     private ProgressDialog pd;
 
 
@@ -43,7 +60,7 @@ public class UploadPdfActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_pdf);
 
-        reference = FirebaseDatabase.getInstance().getReference();
+        databasereference = FirebaseDatabase.getInstance().getReference();
         storageReference = FirebaseStorage.getInstance().getReference();
 
         pd = new ProgressDialog(this);
@@ -51,6 +68,7 @@ public class UploadPdfActivity extends AppCompatActivity {
         addPdf = findViewById(R.id.addPdf);
         pdfTitle = findViewById(R.id.pdfTitle);
         uploadPdfBtn = findViewById(R.id.uploadPdfBtn);
+        pdfTextView = findViewById(R.id.pdfTextView);
 
 
         addPdf.setOnClickListener(new View.OnClickListener() {
@@ -59,8 +77,73 @@ public class UploadPdfActivity extends AppCompatActivity {
                 openGallary();
             }
         });
+        uploadPdfBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                title = pdfTitle.getText().toString();
+                if(title.isEmpty()){
+                    pdfTitle.setError("Empty");
+                    pdfTitle.requestFocus();
+                }else if(pdfData==null){
+                    Toast.makeText(UploadPdfActivity.this, "Please upload pdf", Toast.LENGTH_SHORT).show();
+
+                }else{
+                    uploadPdf();
+                }
+
+            }
+        });
 
     }
+
+    private void uploadPdf() {
+        pd.setTitle("Please wait...");
+        pd.setMessage("Uploading pdf");
+        pd.show();
+        StorageReference reference = storageReference.child("pdf/"+pdfName+"-"+System.currentTimeMillis()+".pdf");
+        reference.putFile(pdfData)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        Task<Uri> uriTask= taskSnapshot.getStorage().getDownloadUrl();
+                        while (!uriTask.isComplete());
+                        Uri uri = uriTask.getResult();
+                        uploadData(String.valueOf(uri));
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                pd.dismiss();
+                Toast.makeText(UploadPdfActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void uploadData(String downloadUrl) {
+        String uniqueKey = databasereference.child("pdf").push().getKey();
+
+        HashMap data = new HashMap();
+        data.put("pdfTitle",title);
+        data.put("pdfUrl",downloadUrl);
+
+        databasereference.child("pdf").child(uniqueKey).setValue(data).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                pd.dismiss();
+                Toast.makeText(UploadPdfActivity.this, "Pdf uploaded successfully", Toast.LENGTH_SHORT).show();
+                pdfTitle.setText("");
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                pd.dismiss();
+                Toast.makeText(UploadPdfActivity.this, "Fail to upload pdf", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
     private void openGallary(){
       Intent intent = new Intent();
       intent.setType("pdf/docs/ppt");
@@ -68,13 +151,29 @@ public class UploadPdfActivity extends AppCompatActivity {
       startActivityForResult(Intent.createChooser(intent,"Select pdf File"), REQ);
     }
 
+    @SuppressLint("Range")
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if(requestCode == REQ && resultCode ==RESULT_OK) {
            pdfData =data.getData();
 
-            Toast.makeText(this, ""+pdfData, Toast.LENGTH_SHORT).show();
+           if(pdfData.toString().startsWith("content://")){
+               Cursor cursor = null;
+               try {
+                   cursor = UploadPdfActivity.this.getContentResolver().query(pdfData,null,null,null,null);
+                   if(cursor != null && cursor.moveToFirst()){
+                       pdfName = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                   }
+               } catch (Exception e) {
+                   e.printStackTrace();
+               }
+
+           }else if(pdfData.toString().startsWith("file://")){
+               pdfName= new File(pdfData.toString()).getName();
+
+           }
+           pdfTextView.setText(pdfName);
         }
 
     }
---
+++ app/src/main/java/com/example/admincollegeapp/faculty/AddTeachers.java
@@ -0,0 +1,16 @@
+package com.example.admincollegeapp.faculty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.admincollegeapp.R;
+
+public class AddTeachers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_teachers);
+    }
+}
